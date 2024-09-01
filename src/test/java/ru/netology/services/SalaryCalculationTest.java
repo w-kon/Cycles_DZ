9667ef4cc7b147a2923e88caa061bff28fa5543a
w-kon <wkon@yandex.ru>
2024-09-01 18:55:27 +0600

    CSV DataTest

--- src/test/java/ru/netology/services/SalaryCalculationTest.java
+++ src/test/java/ru/netology/services/SalaryCalculationTest.java
@@ -2,35 +2,16 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 
 public class SalaryCalculationTest {
-    @Test
-    void OneSalaryCalculation() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/DataTest.txt")
+    void OneSalaryCalculation(int income, int expenses, int threshold, int expected) {
         SalaryCalculation service = new SalaryCalculation();
 
-        // подготавливаем данные:
-        int income = 10000;
-        int expenses = 3000;
-        int threshold = 20000;
-        int expected = 3;
-
-        // вызываем целевой метод:
-        int actual = service.calculate(income, expenses, threshold);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void TwoSalaryCalculation() {
-        SalaryCalculation service = new SalaryCalculation();
-
-        // подготавливаем данные:
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
 
         // вызываем целевой метод:
         int actual = service.calculate(income, expenses, threshold);
